56bfed4612dcf2b2904cd5b440fcb73aad0155b3
Thiago Duvanel <thiago.duvanel.ferreira@live.com>
2025-05-12 21:28:18 -0300

    feat: server: funções para iniciar o servidor e conexão

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,24 +3,35 @@ package server
 import (
 	"log"
 	"net/http"
-	"os"
-	"symphony-api/internal/handlers"
 )
 
-// Run inicializa o servidor HTTP e mapeia as rotas para os handlers.
-// A porta está na variável de ambiente "APP_PORT" ou padrão 8080.
-// A função mapeia a rota "/" para o handler HelloWorldHandler,
-// que responde com "Hello, World!".
-// A função retorna um erro se não for possível iniciar o servidor,
-// caso contrário, o servidor continua em execução.
-func Run() error {
-	port := os.Getenv("APP_PORT")
-	if port == "" {
-		port = "8080"
+type Server struct {
+	port string
+	mux  *http.ServeMux
+}
+
+// NewServer cria uma nova instância do servidor HTTP.
+// O parâmetro 'port' especifica a porta na qual o servidor irá escutar.
+func NewServer(port string) *Server {
+	return &Server{
+		port: port,
+		mux:  http.NewServeMux(),
 	}
+}
 
-	http.HandleFunc("/", handlers.HelloWorldHandler)
+// AddRoute adiciona uma nova rota ao servidor.
+// O parâmetro 'path' especifica o caminho da rota e 'handler' é a função que irá lidar com as requisições para essa rota.
+// O handler deve ser uma função que aceita um http.ResponseWriter e um http.Request como parâmetros.
+func (s *Server) AddRoute(path string, handler http.HandlerFunc) {
+	s.mux.HandleFunc(path, handler)
+}
 
-	log.Printf("Server running on port %s\n", port)
-	return http.ListenAndServe(":"+port, nil)
+// Start inicia o servidor HTTP e escuta na porta especificada.
+// Se houver um erro ao iniciar o servidor, ele será registrado e o programa será encerrado.
+// O servidor irá escutar requisições na porta especificada e encaminhá-las para os manipuladores registrados.
+func (s *Server) Start() {
+	log.Printf("Iniciando servidor em %s...", s.port)
+	if err := http.ListenAndServe(":"+s.port, s.mux); err != nil {
+		log.Fatalf("Não foi possível iniciar o servidor: %s\n", err)
+	}
 }
