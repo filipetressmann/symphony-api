51432ae2a9573365da115a8e118d1644ddd4a2bf
Thiago Duvanel <thiago.duvanel.ferreira@live.com>
2025-06-04 19:42:55 -0300

    refactor: removed comments

--- internal/persistence/connectors/postgres/postgres.go
+++ internal/persistence/connectors/postgres/postgres.go
@@ -10,41 +10,15 @@ import (
 	"github.com/jackc/pgx/v5"
 )
 
-// PostgreConnection defines the interface for interacting with a PostgreSQL database.
-// It provides methods for inserting data into a table and retrieving data with constraints.
-// The Put method inserts a map of data into a specified table and returns the ID of the inserted row.
-// The Get method retrieves rows from a specified table based on given constraints.
 type PostgreConnection interface {
 	Put(data map[string]any, tableName string) (int64, error)
 	Get(constraints map[string]any, tableName string) []map[string]any
 }
 
-// PostgreConnectionImpl is a concrete implementation of the PostgreConnection interface.
-// It holds a pgx.Conn instance which is used to interact with the PostgreSQL database.
-// The Put method constructs an SQL insert statement from the provided data map,
-// executes it, and returns the ID of the newly inserted row.
-// The Get method constructs an SQL select statement with the provided constraints,
-// executes it, and returns the results as a slice of maps, where each map represents a row.
-// The PostgreConnectionImpl struct is designed to be used with the pgx library for PostgreSQL.
-// It is initialized with a connection to the PostgreSQL database, which is established using
-// connection parameters read from environment variables.
 type PostgreConnectionImpl struct {
 	*pgx.Conn
 }
 
-// InitPostgres initializes a new PostgreSQL connection.
-// It reads the connection parameters from environment variables,
-// constructs the connection URL, and creates a PostgreSQL client.
-// If the connection fails, it logs the error and exits the application.
-// The PostgreConnectionImpl struct holds the PostgreSQL connection which can be used
-// to interact with the PostgreSQL database.
-// The connection parameters are:
-// - POSTGRES_USER: The username for the PostgreSQL database (default: "user").
-// - POSTGRES_PASSWORD: The password for the PostgreSQL database (default: "password").
-// - POSTGRES_DB: The name of the PostgreSQL database (default: "symphony").
-// - POSTGRES_HOST: The host of the PostgreSQL database (default: "postgres").
-// - POSTGRES_PORT: The port of the PostgreSQL database (default: "5432").
-// Returns a PostgreConnection instance.
 func NewPostgreConnection() PostgreConnection {
 	user := config.GetEnv("POSTGRES_USER", "user")
 	password := config.GetEnv("POSTGRES_PASSWORD", "password")
@@ -65,13 +39,6 @@ func NewPostgreConnection() PostgreConnection {
 	}
 }
 
-// Put inserts a map of data into a specified table in the PostgreSQL database.
-// It constructs an SQL insert statement from the provided data map,
-// executes it, and returns the ID of the newly inserted row.
-// The data map should contain key-value pairs where keys are column names
-// and values are the corresponding values to be inserted.
-// The tableName parameter specifies the name of the table where the data should be inserted.
-// The method returns the ID of the inserted row and an error if the operation fails.
 func (conn *PostgreConnectionImpl) Put(data map[string]any, tableName string) (int64, error) {
 	var id int64
 
@@ -86,8 +53,6 @@ func (conn *PostgreConnectionImpl) Put(data map[string]any, tableName string) (i
 	return id, err
 }
 
-// getInsertStament constructs an SQL insert statement from the provided data map.
-// It returns the SQL statement as a string and a slice of values to be used as arguments in the query.
 func getInsertStament(data map[string]any, tableName string) (string, []any) {
 	keys := make([]string, 0, len(data))
 	values := make([]any, 0, len(data))
@@ -109,13 +74,6 @@ func getInsertStament(data map[string]any, tableName string) (string, []any) {
 	), values
 }
 
-// Get retrieves rows from a specified table in the PostgreSQL database based on given constraints.
-// It constructs an SQL select statement with the provided constraints,
-// executes it, and returns the results as a slice of maps, where each map represents a row.
-// The constraints map should contain key-value pairs where keys are column names
-// and values are the corresponding values to filter the rows.
-// The tableName parameter specifies the name of the table from which to retrieve the data.
-// The method returns a slice of maps representing the rows that match the constraints.
 func (conn *PostgreConnectionImpl) Get(constraints map[string]any, tableName string) []map[string]any {
 	sql, args := getSelectWthConstraintsQuery(constraints, tableName)
 
@@ -138,8 +96,6 @@ func (conn *PostgreConnectionImpl) Get(constraints map[string]any, tableName str
 	return result
 }
 
-// rowsToMaps converts pgx.Rows to a slice of maps, where each map represents a row.
-// It retrieves the column names from the rows and maps each row's values to the corresponding column names.
 func rowsToMaps(rows pgx.Rows) ([]map[string]any, error) {
 	defer rows.Close()
 
@@ -168,8 +124,6 @@ func rowsToMaps(rows pgx.Rows) ([]map[string]any, error) {
 	return results, nil
 }
 
-// getRowColumns retrieves the column names from pgx.Rows.
-// It returns a slice of strings representing the column names.
 func getRowColumns(rows pgx.Rows) []string {
 	fieldDescriptions := rows.FieldDescriptions()
 	columns := make([]string, len(fieldDescriptions))
@@ -180,8 +134,6 @@ func getRowColumns(rows pgx.Rows) []string {
 	return columns
 }
 
-// getSelectWthConstraintsQuery constructs an SQL select statement with the provided constraints.
-// It returns the SQL statement as a string and a slice of values to be used as arguments in the query.
 func getSelectWthConstraintsQuery(constraints map[string]any, tableName string) (string, []any) {
 	values := make([]any, 0, len(constraints))
 	constraintList := make([]string, 0, len(constraints))
@@ -200,8 +152,6 @@ func getSelectWthConstraintsQuery(constraints map[string]any, tableName string) 
 		), values
 }
 
-// joinComma joins a slice of strings with commas.
-// It returns a single string with the elements of the slice separated by commas.
 func joinComma(values []string) string {
 	return strings.Join(values, ",")
 }
--- internal/persistence/model/user.go
+++ internal/persistence/model/user.go
@@ -6,14 +6,6 @@ import (
 	"time"
 )
 
-// User represents a user in the system.
-// It contains fields for user ID, username, full name, email, registration date,
-// birth date, and telephone number.
-// The User struct is designed to be used with JSON serialization and deserialization.
-// The `json` tags specify how the fields should be serialized to and from JSON.
-// The `Register_date` field is automatically set to the current time when a new User is created.
-// The `Birth_date` field is expected to be provided in the format of a time.Time object.
-// The `UserId` field is an int64 that uniquely identifies the user.
 type User struct {
 	UserId int64
 	Username string `json:"username"`
@@ -24,10 +16,6 @@ type User struct {
 	Telephone string `json:"telephone"`
 }
 
-// NewUser creates a new User instance with the provided parameters.
-// It initializes the UserId, Username, Fullname, Email, Birth_date, and Telephone fields.
-// The Register_date field is set to the current time.
-// The function returns a pointer to the newly created User instance.
 func NewUser(
 	userId int64,
 	username string,
@@ -47,9 +35,6 @@ func NewUser(
 	}
 }
 
-// ToMap converts the User instance to a map[string]any.
-// It returns a map where the keys are the field names and the values are the corresponding field values.
-// This method is useful for serializing the User instance to a format that can be easily converted to JSON or used in other contexts.
 func (user *User) ToMap() map[string]any {
 	return map[string]any{
 		"id": user.UserId,
@@ -62,19 +47,12 @@ func (user *User) ToMap() map[string]any {
 	}
 }
 
-// UserFromRequest decodes a JSON request body into a User instance.
-// It reads the request body, unmarshals the JSON data into a User struct,
-// and returns a pointer to the User instance along with any error encountered during the decoding process.
 func UserFromRequest(request *http.Request) (*User, error) {
 	var user User
     err := json.NewDecoder(request.Body).Decode(&user)
     return &user, err
 }
 
-// MapToUser converts a map[string]any to a User instance.
-// It expects the map to contain keys corresponding to the User fields.
-// The function retrieves the values from the map, casts them to the appropriate types,
-// and returns a pointer to a new User instance.
 func MapToUser(data map[string]any) *User {
 	return &User{
 		UserId: data["id"].(int64),
