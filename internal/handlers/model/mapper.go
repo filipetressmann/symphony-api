8d3728f4fae6d15b60487712dbcba58388164f42
octopus-me <guiwallace96@gmail.com>
2025-06-08 14:42:59 -0300

    refactor: enhance error logging in MapRequest and MustEncodeAnswer functions

--- internal/handlers/model/mapper.go
+++ internal/handlers/model/mapper.go
@@ -8,20 +8,22 @@ import (
 
 func MapRequest[T any](r *http.Request) (*T, error) {
 	request := new(T)
-    err := json.NewDecoder(r.Body).Decode(request)
+	err := json.NewDecoder(r.Body).Decode(request)
 	if err != nil {
-		log.Println(err.Error())
+		log.Printf("Error decoding request body: %v", err)
+		log.Printf("Request body: %v", r.Body)
+		return nil, err
 	}
 
-    return request, err
+	return request, nil
 }
 
 func MustEncodeAnswer(data any, w http.ResponseWriter) {
 	err := json.NewEncoder(w).Encode(data)
-	
+
 	if err != nil {
 		log.Printf("Error encoding answer: %s", err)
-		http.Error(w, "Error while encoding answer" , http.StatusInternalServerError)
-        return
+		http.Error(w, "Error while encoding answer", http.StatusInternalServerError)
+		return
 	}
 }
