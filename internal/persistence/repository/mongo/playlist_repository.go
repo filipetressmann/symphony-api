3f5ea7a2eece79756b458dc89539b443ddd3d48a
octopus-me <guiwallace96@gmail.com>
2025-06-15 17:44:51 -0300

    feat: implement PlaylistRepository for MongoDB with methods to insert and retrieve playlists

---
+++ internal/persistence/repository/mongo/playlist_repository.go
@@ -0,0 +1,55 @@
+package mongo_repository
+
+import (
+	"context"
+	local_mongo "symphony-api/internal/persistence/connectors/mongo"
+
+	"symphony-api/internal/persistence/model"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type PlaylistRepository struct {
+	collection *mongo.Collection
+}
+
+func NewPlaylistRepository(conn *local_mongo.MongoConnection) *PlaylistRepository {
+	coll := conn.GetCollection("symphony", "playlists")
+	return &PlaylistRepository{collection: coll}
+}
+
+func (r *PlaylistRepository) InsertPlaylist(ctx context.Context, playlist model.Playlist) (*mongo.InsertOneResult, error) {
+	return r.collection.InsertOne(ctx, playlist)
+}
+
+func (r *PlaylistRepository) GetPlaylistByID(ctx context.Context, id primitive.ObjectID) (*model.Playlist, error) {
+	var playlist model.Playlist
+	err := r.collection.FindOne(ctx, bson.M{"_id": id}).Decode(&playlist)
+	if err != nil {
+		return nil, err
+	}
+	return &playlist, nil
+}
+
+func (r *PlaylistRepository) GetPlaylistsByUserID(ctx context.Context, userID string) ([]model.Playlist, error) {
+	cursor, err := r.collection.Find(ctx, bson.M{"user_id": userID})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	var playlists []model.Playlist
+	for cursor.Next(ctx) {
+		var playlist model.Playlist
+		if err := cursor.Decode(&playlist); err != nil {
+			return nil, err
+		}
+		playlists = append(playlists, playlist)
+	}
+
+	if err := cursor.Err(); err != nil {
+		return nil, err
+	}
+	return playlists, nil
+}
