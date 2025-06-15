0946fcf8cd133c769cbb3b6616e72d499f1f1fd4
octopus-me <guiwallace96@gmail.com>
2025-06-15 09:54:01 -0300

    feat: add MongoDB models for Song, Artist, and Playlist

---
+++ internal/persistence/model/mongo_models.go
@@ -0,0 +1,48 @@
+package model
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Song struct {
+	ID          primitive.ObjectID 		`bson:"_id"`
+	IDSpotify   string             		`bson:"id_spotify,omitempty"`
+	Title       string             		`bson:"title,omitempty"`
+	Duration    int32              		`bson:"duration,omitempty"`
+	ArtistID    primitive.ObjectID 		`bson:"artist_id,omitempty"`
+	Genre       string             		`bson:"genre,omitempty"`
+	ReseaseYear int32          			`bson:"release_year,omitempty"`
+	Album       string         			`bson:"album,omitempty"`
+	URLSpotify  string         			`bson:"url_spotify,omitempty"`
+}
+
+type Artist struct {
+	ID          primitive.ObjectID `bson:"_id"`
+	IDSpotify   string             `bson:"id_spotify,omitempty"`
+	Name        string             `bson:"name,omitempty"`
+	Description string             `bson:"description,omitempty"`
+	Country     string             `bson:"country,omitempty"`
+	ImageURL    string             `bson:"image_url,omitempty"`
+	Biography   string         		`bson:"biography,omitempty"`
+	Genres      []string       		`bson:"genres,omitempty"`
+}
+
+type Playlist struct {
+	ID          primitive.ObjectID 	`bson:"_id"`
+	Name        string             	`bson:"name,omitempty"`
+	Public      bool               	`bson:"public,omitempty"`
+	UserID      string             	`bson:"user_id,omitempty"`
+	IDSpotify   string             	`bson:"id_spotify,omitempty"`
+	Title       string             	`bson:"title,omitempty"`
+	Description string             	`bson:"description,omitempty"`
+	ImageURL    string             	`bson:"image_url,omitempty"`
+	CreatedAt   time.Time      		`bson:"created_at,omitempty"`
+	UpdatedAt   time.Time      		`bson:"updated_at,omitempty"`
+	Likes       []string       		`bson:"likes,omitempty"`
+	Songs       []struct {
+		SongID primitive.ObjectID 		`bson:"song_id,omitempty"`
+		Order  int32              		`bson:"order,omitempty"`
+	} `bson:"songs,omitempty"`
+}
